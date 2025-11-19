42b06e28c58dcc86305c8bd99ef564bb38ee6e8e
rashichouksey2025-git <your_email_for_rashichouksey2025@gmail.com>
2025-11-19 18:32:20 +0530

    remove error of tsx

--- src/main.js
+++ src/main.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 const react_1 = require("react");
 const client_1 = require("react-dom/client");
-require("./index.css");
 const App_1 = __importDefault(require("./App"));
+require("./index.css");
 (0, client_1.createRoot)(document.getElementById('root')).render((0, jsx_runtime_1.jsx)(react_1.StrictMode, { children: (0, jsx_runtime_1.jsx)(App_1.default, {}) }));
